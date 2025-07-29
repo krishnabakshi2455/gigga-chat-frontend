e3a357a3ea0e37292d91805151664a1515f3e802
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2025-07-29 21:42:48 +0530

    commit

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -11,14 +11,20 @@ import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 
-
+export type RootStackParamList = {
+    Login: undefined;
+    Register: undefined;
+    Home: undefined;
+};
+type NavProp = NativeStackNavigationProp<RootStackParamList>;
 
 const LoginScreen = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const navigation = useNavigation();
+    const navigation = useNavigation<NavProp>();
     useEffect(() => {
         const checkLoginStatus = async () => {
             try {
@@ -30,7 +36,7 @@ const LoginScreen = () => {
                     // token not found , show the login screen itself
                 }
             } catch (error) {
-                console.log("error", error);
+                // console.log("error", error);
             }
         };
 
@@ -45,7 +51,7 @@ const LoginScreen = () => {
         axios
             .post("http://localhost:8000/login", user)
             .then((response) => {
-                console.log(response);
+                // console.log(response);
                 const token = response.data.token;
                 AsyncStorage.setItem("authToken", token);
 
@@ -53,7 +59,7 @@ const LoginScreen = () => {
             })
             .catch((error) => {
                 Alert.alert("Login Error", "Invalid email or password");
-                console.log("Login Error", error);
+                // console.log("Login Error", error);
             });
     };
     return (
@@ -65,13 +71,13 @@ const LoginScreen = () => {
                     className="mt-24 justify-center items-center flex"
                 >
                     <Text 
-                        className="text-lg font-semibold text-blue-600"
+                        className="text-lg font-semibold text-white"
                     >
                         Sign In
                     </Text>
 
                     <Text 
-                        className="text-lg font-semibold mt-4 text-blue-600"
+                        className="text-lg font-semibold mt-4 text-white"
                     >
                         Sign In to Your Account
                     </Text>
@@ -88,20 +94,17 @@ const LoginScreen = () => {
                         <TextInput
                             value={email}
                             onChangeText={(text) => setEmail(text)}
-                            style={{
-                                fontSize: email ? 18 : 18,
-                                borderBottomColor: "gray",
-                                borderBottomWidth: 1,
-                                marginVertical: 10,
-                                width: 300,
-                            }}
-                            placeholderTextColor={"black"}
-                            placeholder="enter Your Email"
+                            className={`${email?"text-lg":" text-lg"} text-white border-b-2 my-3 w-72 border-gray-400`}
+                            placeholderTextColor={"grey"}
+                            placeholder="Enter Your Email"
                         />
                     </View>
 
-                    <View style={{ marginTop: 10 }}>
-                        <Text style={{ fontSize: 18, fontWeight: "600", color: "gray" }}>
+                    <View 
+                    className="mt-3">
+                        <Text 
+                            className="text-lg font-semibold text-blue-700"
+                        >
                             Password
                         </Text>
 
@@ -109,29 +112,15 @@ const LoginScreen = () => {
                             value={password}
                             onChangeText={(text) => setPassword(text)}
                             secureTextEntry={true}
-                            style={{
-                                fontSize: email ? 18 : 18,
-                                borderBottomColor: "gray",
-                                borderBottomWidth: 1,
-                                marginVertical: 10,
-                                width: 300,
-                            }}
-                            placeholderTextColor={"black"}
+                            className={`${email ? "text-lg" : " text-lg"} text-white border-b-2 my-3 w-72 border-gray-400`}
+                            placeholderTextColor={"grey"}
                             placeholder="Passowrd"
                         />
                     </View>
 
                     <Pressable
                         onPress={handleLogin}
-                        style={{
-                            width: 200,
-                            backgroundColor: "#4A55A2",
-                            padding: 15,
-                            marginTop: 50,
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            borderRadius: 6,
-                        }}
+                        className="w-52 bg-blue-600 mt-12 mx-auto p-4 rounded-md"
                     >
                         <Text
                             style={{
@@ -147,10 +136,10 @@ const LoginScreen = () => {
 
                     <Pressable
                         onPress={() => navigation.navigate("Register")}
-                        style={{ marginTop: 15 }}
+                        className="mt-4"
                     >
                         <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
-                            Dont't have an account? Sign Up
+                            Dont't have an account? <Text className="text-blue-600">Sign Up</Text>
                         </Text>
                     </Pressable>
                 </View>
