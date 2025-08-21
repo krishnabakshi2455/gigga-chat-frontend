40fcf902f3dd297d236f4811fda3956342e6c130
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2025-08-21 14:40:17 +0530

    commit

---
+++ config.ts
@@ -0,0 +1,41 @@
+const config = {
+  // BACKEND_URL: 'http://localhost:8000',
+  BACKEND_URL: 'http://192.168.31.144:8000',
+  SHA1_Fingerprint: "73:50:45:5D:F5:31:32:C8:67:5D:77:01:90:0F:9D:B0:2B:60:BD:AB",
+  Android_Client_ID: "1020305574623-um4bge5gt2gguijql18btp2favachc46.apps.googleusercontent.com",
+  Web_Client_ID: "1020305574623-roke1rqa6e6dm7masu8n5dpf895g42hc.apps.googleusercontent.com",
+};
+
+export default config;
+
+
+
+
+// 📝  Android application id Learn more: https://expo.fyi/android-package
+
+// com.krishnabakshi.giggachat
+{/*
+eas credentials
+use this command to check if the sh1 is right that is very important
+*/}
+
+{/*
+EAS (Expo Application Services) → Expo’s cloud service that handles building, signing, and deploying React Native apps without requiring local Android Studio or Xcode setup.
+
+SHA-1 → A unique cryptographic fingerprint of your Android signing key that Google uses to verify your app for services like Google Sign-In.
+
+Android vs iOS OAuth → Android uses package name + SHA-1, while iOS uses bundle ID; each platform needs its own OAuth client in Google Cloud.    
+*/}
+
+{/*
+Authorized JavaScript origins
+For use with requests from a browser
+URIs 1 
+https://auth.expo.io
+
+
+
+  Authorized redirect URIs
+  https://auth.expo.io/@krishna/gigga-chat
+ */}
+
