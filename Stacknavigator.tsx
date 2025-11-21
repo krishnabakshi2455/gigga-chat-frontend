0bb9d494ed9251b1d093d7ec507eb17b84f4f818
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2025-11-21 07:16:10 +0530

    commit

--- Stacknavigator.tsx
+++ Stacknavigator.tsx
@@ -1,4 +1,3 @@
-// Add this to your Stacknavigator.tsx
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
